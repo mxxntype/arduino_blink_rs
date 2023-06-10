ee86d30c3f2ed1510199aa328593f1e9ee1fac7c
mxxntype <mxxntype@Nox>
2023-06-10 15:41:34 +0300

    feat: add blinking code
    - Added the actual `blinking` code.
      Does not compile due to an LLVM issue, waiting for upstream updates...
    
    Signed-off-by: mxxntype <59417007+mxxntype@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+/*!
+ * Blink the builtin LED - the "Hello World" of embedded programming.
+ */
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    // D13 is also connected to an onboard LED marked "L"
+    let mut led = pins.d13.into_output();
+    led.set_high();
+
+    loop {
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(100);
+        led.toggle();
+        arduino_hal::delay_ms(800);
+    }
 }
