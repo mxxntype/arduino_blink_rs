4c731cc64463a69fa245622fc7ff4df9a3282740
mxxntype <mxxntype@Nox>
2023-06-10 15:38:48 +0300

    feat: add flake, target, dependencies
    - Initialized a flake.
    - Added github:Rahix/avr-hal to crate dependencies
    - Added panic-halt to crate dependencies
    - Added target config in avr-atmega328p.json
    - Configured cargo to use the target file
    
    Signed-off-by: mxxntype <59417007+mxxntype@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Arduino Uno D13 blinker in Rust with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+    {
+      devShells."x86_64-linux".default = let
+        overlays = [ inputs.rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          inherit overlays;
+        };
+        avr = with pkgs.pkgsCross.avr.buildPackages; [
+          binutils
+          gcc
+          avrdude
+        ];
+      in pkgs.mkShell {
+        name = "arduino_blink_rs_shell";
+        buildInputs = avr ++ [
+          (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            extensions = [ "rust-src" ];
+          }))
+        ];
+      };
+    };
+}
